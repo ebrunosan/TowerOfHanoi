669d4edf88cf8e00d615321974979b1c46f7ca9f
Bruno <ebrunosan@gmail.com>
2018-04-28 19:32:14 -0400

    Remove jscript events from HTML

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 // Writen on: Nov 23, 2016
 // Writen by: Bruno Santos
 
-"use strict";
-var c, ctx;                     // used to draw canvas
-
-const GAME_LEVEL = 3;           // TODO: define object
-
-const BASIC_LEVEL = 3;           // TODO: define object
+'use strict';
+const BASIC_LEVEL = 3;
 const BASIC_MOVEMENTS = 7;
-const HARD_LEVEL = 4;           // TODO: define object
+const HARD_LEVEL = 4;
 const HARD_MOVEMENTS = 15;
-const PRO_LEVEL = 4;           // TODO: define object
+const PRO_LEVEL = 5;
 const PRO_MOVEMENTS = 31;
+
+const IDX_TOWER_A = 0;
+const IDX_TOWER_B = 1;
+const IDX_TOWER_C = 2;
 
 const RING_HEIGHT = 20;         // fixed high of rings
 
-var towerArr = [5];             // array length of max level which contains tower's objects 
-var countPlayersMovement = 0;   // general counter to show
-var bestMovementsToSolve = 0;   // it's a counter of an array movementsToSolve
+let towerArr = [5];             // array length of max level which contains tower's objects 
+let countPlayersMovement = 0;   // general counter to show
+let bestMovementsToSolve = 0;   // it's a counter of an array movementsToSolve
+
+let c, ctx;                     // used to draw canvas
+c = document.getElementById("myCanvas");
+ctx = c.getContext("2d");
+
+let gameLevel;
+let cmbGameLevel = document.getElementById("gameMode");
+cmbGameLevel.onchange=function onchange(event) {reset()};
+
+document.getElementById("reset").onclick=function onclick(event) {reset()};
+
+window.onload = reset;
 
 /* ----------------
 * OBJECT: used to store Rings objects and act on each one
@@ -67,7 +79,7 @@ function Tower(centerX) {
     };
     // It stack "n" object Rings over tower based on a level selected
     this.initialState = function(numRings) {
-        for (var i=numRings; i>0; i--) {
+        for (let i=numRings; i>0; i--) {
             this.receiveNewRing(new Ring(i));
         }
     }
@@ -77,7 +89,7 @@ function Tower(centerX) {
 */
 function Ring(size) {
     this.size = size;
-    switch (this.size) {            // stabilish visual properties of each ring size
+    switch (this.size) {            // sets visual properties of each ring size
         case 5:
             this.width = 220;
             this.color = "purple";
@@ -129,37 +141,36 @@ function Ring(size) {
 * FUNCTION: It interact with rings if it's clicked over tower's coordinate
 */
 function getPositionClicked(event) {
-    var xClicked = event.offsetX;
-    var yClicked = event.offsetY;
+    let xClicked = event.offsetX;
+    let yClicked = event.offsetY;
+    
+    showMsg("");
 
+    // TODO improve the settings of those 3 IDX as parameters below
     if (xClicked >= 35 && xClicked <= 255) {            // if clicked on TowerA
-        workOnMovementRing(0, 1, 2);
+        workOnMovementRing(IDX_TOWER_A, IDX_TOWER_B, IDX_TOWER_C);
+        
     } else if (xClicked >= 285 && xClicked <= 505) {    // if clicked on TowerB
-        workOnMovementRing(1, 0, 2);
+        workOnMovementRing(IDX_TOWER_B, IDX_TOWER_A, IDX_TOWER_C);
+        
     } else if (xClicked >= 540 && xClicked <= 760) {    // if clicked on TowerC
-        workOnMovementRing(2, 0, 1);
+        workOnMovementRing(IDX_TOWER_C, IDX_TOWER_A, IDX_TOWER_B);
     }
 }
 /* ----------------
 * FUNCTION: It performs a movement of a ring based on the tower clicked
 */
 function workOnMovementRing(idxTowerClicked, idxOtherTower1, idxOtherTower2) {
-    var idxTowerSrc, idxTowerDst;
-    
     if (towerArr[idxTowerClicked].isThereARingOnTop) {
         towerArr[idxTowerClicked].moveRingDown();
         
     } else if (towerArr[idxOtherTower1].isThereARingOnTop) {
-        idxTowerSrc = idxOtherTower1;
-        idxTowerDst = idxTowerClicked;
-        moveRing(idxTowerSrc, idxTowerDst);
+        moveRing(idxOtherTower1, idxTowerClicked);
         
     } else if (towerArr[idxOtherTower2].isThereARingOnTop) {
-        idxTowerSrc = idxOtherTower2;
-        idxTowerDst = idxTowerClicked;
-        moveRing(idxTowerSrc, idxTowerDst);
+        moveRing(idxOtherTower2, idxTowerClicked);
         
-    } else {
+    } else if (towerArr[idxTowerClicked].ringsArr.length > 0) {
         towerArr[idxTowerClicked].moveRingUp();
     }
     return;
@@ -168,7 +179,7 @@ function workOnMovementRing(idxTowerClicked, idxOtherTower1, idxOtherTower2) {
 * FUNCTION: It performs a movement of a ring from a source Tower to a Destination
 */
 function moveRing(idxTowerSrc, idxTowerDst) {
-    var ring2Move = towerArr[idxTowerSrc].getHigherRing();
+    let ring2Move = towerArr[idxTowerSrc].getHigherRing();
     
     if (towerArr[idxTowerDst].isAble2ReceiveRing(ring2Move)) {
         towerArr[idxTowerSrc].releaseRing();
@@ -176,35 +187,55 @@ function moveRing(idxTowerSrc, idxTowerDst) {
         countPlayersMovement++;
         checkEndOfGame(idxTowerDst);
     } else {
-        showAlert("<h2>Ops!</h2>Movement not allowed!<br>You can't move a higher ring over a lower one.<br>Click on <strong>instructions</strong> to check it out all rules if you want.", "hideAlert");
+        showMsg("Movement not allowed!");
     }
 }
 /* ----------------
 * FUNCTION: Shows message and reset the game if users reach end of the game
 */
 function checkEndOfGame(idxTowerDst) {
-    if (idxTowerDst != 0 && towerArr[idxTowerDst].ringsArr.length == GAME_LEVEL) {
-        var msg = "";
+    let msg = "";
+    if (idxTowerDst != 0 && towerArr[idxTowerDst].ringsArr.length == gameLevel) {
         if (countPlayersMovement == bestMovementsToSolve) {
-            msg = "<h2>Perfect!</h2>You reached the goal pretty well!";
-            msg += "<br>I wish you enjoyed the game! <br>Challenge yourself selecting a higher level of the game."
+            msg = "Perfect! Goal reached with mimimum ";
         } else {
-            msg = "<h2>You could be better!</h2>You reached the goal with more movements than expected."; 
-            msg+= "<br>Let's try it again with "+bestMovementsToSolve+" movements.";
+            msg = "Well done! You should try with ";
         }
-        showAlert(msg, "reset");
+        msg += bestMovementsToSolve+" moves.";
+        c.onclick="";       // remove canvas event
     }
+    showMsg(msg);
 }
 /* ----------------
 * FUNCTION: It reset the screen drawing towers and loading rings based on a level selected
 */
 function reset() {
-    c = document.getElementById("myCanvas");
-    ctx = c.getContext("2d");
-    var towerCenterX;
+    drawCanvas();
+    c.onclick=function onclick(event) {getPositionClicked(event)};
 
-    document.getElementById("alert").style.zIndex="-1";
-    activateMoveGame(true);
+    // It fills all rings at tower "A" based on a level selected
+    gameLevel = +cmbGameLevel.options[cmbGameLevel.selectedIndex].value;
+    towerArr[0].initialState(gameLevel);
+    countPlayersMovement = 0;
+    switch (gameLevel) {
+        case PRO_LEVEL: 
+            bestMovementsToSolve = PRO_MOVEMENTS;
+            break;
+        case HARD_LEVEL: 
+            bestMovementsToSolve = HARD_MOVEMENTS;
+            break;
+        default:
+            bestMovementsToSolve = BASIC_MOVEMENTS;
+            break;
+    }
+    showMsg("Good luck!");
+    return;
+}
+/* ----------------
+* FUNCTION: It presents a div id=alert and presents a message by its caller
+*/
+function drawCanvas() {
+    let towerCenterX;
 
     // Drawing tower's and loading tower's Object into global towerArr[]
     ctx.fillStyle = "black";
@@ -227,58 +258,10 @@ function reset() {
     towerArr[2] = new Tower(towerCenterX);
     ctx.fillRect(towerCenterX, 150, 10, 200);
     ctx.fillText('Tower C',600,60);
-
-    // It fills all rings at tower "A" based on a level selected
-    initialState(GAME_LEVEL);
-}
-/* ----------------
-* FUNCTION: 1) It builds start rings into Tower A
-*           2) fills solution array
-*           3) shows the best numbers of movements
-*/
-function initialState(levelGame) {
-    towerArr[0].initialState(levelGame);
-    countPlayersMovement = 0;
-    switch (GAME_LEVEL) {
-        case PRO_LEVEL: 
-            bestMovementsToSolve = PRO_MOVEMENTS;
-            break;
-        case HARD_LEVEL: 
-            bestMovementsToSolve = HARD_MOVEMENTS;
-            break;
-        default:
-            bestMovementsToSolve = BASIC_MOVEMENTS;
-            break;
-    }
-    return;
-}
-/* ----------------
-* FUNCTION: It hides a div id=alert
-*/
-function hideAlert() {
-    document.getElementById("alert").style.zIndex="-1";
-    activateMoveGame(true);
 }
 /* ----------------
 * FUNCTION: It presents a div id=alert and presents a message by its caller
 */
-function showAlert(msg, nextAction) {
-    document.getElementById("alertMsg").innerHTML = msg;
-    document.getElementById("alert").style.zIndex="1";
-    if (nextAction=="reset"){
-        document.getElementById("msgBoxDialogBtn").onclick = function onclick(event) {reset(event)};
-    } else {
-        document.getElementById("msgBoxDialogBtn").onclick = function onclick(event) {hideAlert(event)};
-    }
-    activateMoveGame(false);
-}
-/* ----------------
-* FUNCTION: It enable/disable click on tower when shows a dialog box
-*/
-function activateMoveGame(bValue) {
-    if (bValue){
-        document.getElementById("myCanvas").onclick=function onclick(event) {getPositionClicked(event)};
-    } else {
-        document.getElementById("myCanvas").onclick="";
-    }
+function showMsg(msg) {
+    document.getElementById("gameMsg").innerHTML = msg;
 }
